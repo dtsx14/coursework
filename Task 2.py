dd339ca7ee7c0212c20b5237cc5af8af8f2178a1
dtsx14 <dylan.saberton@gmail.com>
2016-11-29 13:59:46 +0000

    Changed arrangement of quantity
    Still needs doing:
    -Search file and output line,
    -At the end, give the user the option to search for another code
    -Add ability to add products to the reorder file

--- Task 2.py
+++ Task 2.py
@@ -6,21 +6,22 @@ while cont == ('y'):
     print ('---------------------------------------------------')
     #Records the users input into a variable called gtinCode
     gtinCode = input()
+    print ('And how many of those would you like?')
+    quantity = input()
     #Figures out the length of the string that the user has entered
     lenofstring = len(gtinCode)
     if lenofstring is not 8 :
         print ('Im sorry, the code that you entered was not valid, please try again')
         cont = ('y')
     else:
-        print ('Thanks, now please enter the quantity of the product')
-        quantity = input()
         print("------Loading the stock file - this won't take a minute------")
         stockfile=open("//Client/N$/GCSE Computer Science/A453/Python/Stockfile.txt", "r") #this opens the text file
         stockline=stockfile.readlines() #reads the file and stores it as the variable 'stockfile' stockfile.close() closes the file)  
         if (gtinCode) in stockfile:
             productDescriptions = open("Products.txt", "w")
-            
         else:
             print ("I'm sorry, the product that you asked for does not exist, please try again")
             cont ('y')
-    
+else:
+    print ('Thanks, we hope you are satisfied with your purchase')
+    quit
